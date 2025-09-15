88d8ac5cd8f1f62bdef90be9d6c989ee3e5f01a3
alex707 <alex.grigorenko2942@gmail.com>
2025-09-15 15:37:25 +0300

    getting of vacancy

--- app/services/head_hunter_client.rb
+++ app/services/head_hunter_client.rb
@@ -6,4 +6,20 @@ class HeadHunterClient
 
     JSON.parse(response.body.to_s) if response.status == 200
   end
+
+  def get_vacancy_data(vacancy_hh_id)
+    link = File.join(HH_URL, 'vacancies', vacancy_hh_id.to_s)
+
+    response = HTTPX.get(link)
+
+    JSON.parse(response.body.to_s) if response.status == 200
+  end
+
+  def get_vacancies_data(params)
+    link = File.join(HH_URL, 'vacancies')
+
+    response = HTTPX.get(link, params: params)
+
+    JSON.parse(response.body.to_s) if response.status == 200
+  end
 end
